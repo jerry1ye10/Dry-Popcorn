2f56d5a8982886d8e2ceb8c66aa5ce020f53c29e
Jason Lin <jasonlin43212@gmail.com>
2018-12-03 13:07:05 -0500

    added docstring and fixed css

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def is_logged_in():
 
 @app.route("/") #Linking a function to a route
 def home():
+    '''Renders the homepage with random cities.'''
     cities = ["New York", "Los Angeles", "Miami", "Beijing", "Milan",
               "Hanoi", "Dubai", "Buenos Aires", "Saint Petersburg", "Caracas",
               "Cairo", "Munich", "Budapest", "Birmingham", "Alexandria",
@@ -56,16 +57,22 @@ def home():
 
 @app.route("/login")
 def login():
+    '''Redirects to the homepage if the user is logged in. Displays the login page
+    if the user is not logged in.'''
     if (is_logged_in()):
         return redirect(url_for("home"))
     return render_template("login.html");
 
 @app.route("/register")
 def register():
+    '''Display the register page.'''
     return render_template("register.html", isLoggedIn = is_logged_in())
 
 @app.route("/auth", methods=["POST"])
 def authenticate():
+    '''Handles the information that the user submits for either logging in or
+    registering. It will redirect the user to the appropriate pages and flash
+    messages if there are errors.'''
     submit_type = frequest.form.get("submit")
     username_list = db.get_username_list()
     if (submit_type == "Register"):
@@ -96,17 +103,22 @@ def authenticate():
 
 @app.route("/logout")
 def logout():
-    session.pop("username")
-    return redirect(url_for("login"))
+    '''Removes the current session and redirects users back to login page.'''
+    if (is_logged_in()):
+        session.pop("username")
+        return redirect(url_for("login"))
+    return redirect(url_for("home"))
 
 @app.route("/favorites")
 def favorites():
+    '''Redirects the user to the login page if it isn't logged in.'''
     if (not is_logged_in()):
         return redirect(url_for("login"))
     return render_template("favorites.html", isLoggedIn = is_logged_in())
 
 @app.route("/search", methods=["GET"])
 def search():
+    '''Renders the search page.'''
     return render_template("search.html", isLoggedIn = is_logged_in())
 
 if __name__ == "__main__":
